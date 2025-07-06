1d14f22aa86a47188980ee1f0d8ed26b2d55b639
uPedroLima11 <pedromendeslima2016@gmail.com>
2025-07-05 22:20:39 -0300

    fix: rotas i18n

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -1,6 +1,5 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 import settingsEn from '../public/locales/en/settings.json';
 import sidebarEn from '../public/locales/en/sidebar.json';
@@ -37,7 +36,6 @@ import clientesEn from '../public/locales/en/clientes.json';
 
 if (!i18n.isInitialized) {
   i18n
-    .use(LanguageDetector)
     .use(initReactI18next)
     .init({
       ns: ['settings', 'sidebar', 'dashboard', 'produtos', 'usuarios','conta','empresa','criarempresa','vendas','ativacao','fornecedores', 'suporte', 'logs', 'esqueci' , 'alteracao' , 'clientes'],
